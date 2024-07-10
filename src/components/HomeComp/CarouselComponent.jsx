09344bc5cfe68e1374ca1ba71ed790a7559e13b1
vijay <vijaymano0501@gmail.com>
2024-07-10 12:18:16 +0530

    finalized carrousel

--- src/components/HomeComp/CarouselComponent.jsx
+++ src/components/HomeComp/CarouselComponent.jsx
@@ -2,8 +2,8 @@ import { Carousel } from "flowbite-react";
 
  function CarouselComponent() {
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel pauseOnHover>
+    <div className="h-[500px] sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel pauseOnHover >
         <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
