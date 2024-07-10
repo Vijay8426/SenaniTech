ee8d2d478838bf82c77b372767f1e53177e44ffa
vijay <vijaymano0501@gmail.com>
2024-07-10 11:20:43 +0530

    removed tailwind cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src//*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      screens: {
-        '3xl': '1920px',
-        // Add more custom breakpoints as needed
-      },
+      
     },
   },
   plugins: [],
