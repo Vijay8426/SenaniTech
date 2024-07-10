718a564591935184ca05af85ccf47d0e40727256
vijay <vijaymano0501@gmail.com>
2024-07-10 13:03:44 +0530

    final home page

--- src/components/HomeComp/CarouselComponent.jsx
+++ src/components/HomeComp/CarouselComponent.jsx
@@ -2,16 +2,19 @@ import { Carousel } from "flowbite-react";
 import Manufacturing from '../../image-org/home/Carousel-images/Manufacturing_Image 2.jpg';
 import Production from '../../image-org/home/Carousel-images/Product Engineering  - Image.jpg';
 import PCB from '../../image-org/home/Carousel-images/Product Engineering Image_2.jpg';
- function CarouselComponent() {
-  return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96" style={{ height: '500px' }}>
-      <Carousel pauseOnHover >
-        <img src={Manufacturing} alt="..." />
-        <img src={Production} alt="..." />
-        <img src={PCB} alt="..." />
 
-      </Carousel>
+function CarouselComponent() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 pt-8 sm:pt-10 lg:pt-12 xl:py-10 2xl:py-20 bg-gray-100">
+      <div className="h-64 sm:h-72 md:h-80 lg:h-96 xl:h-[500px] 2xl:h-[600px]">
+        <Carousel pauseOnHover>
+          <img className="object-cover w-full h-full" src={Manufacturing} alt="Manufacturing" />
+          <img className="object-cover w-full h-full" src={Production} alt="Production" />
+          <img className="object-cover w-full h-full" src={PCB} alt="PCB" />
+        </Carousel>
+      </div>
     </div>
   );
 }
+
 export default CarouselComponent;
