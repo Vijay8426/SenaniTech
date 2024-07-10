bdb7951b8b1ec508cac9007f26e13da65e08d5e4
vijay <vijaymano0501@gmail.com>
2024-07-10 12:42:27 +0530

    carousel image

--- src/components/HomeComp/CarouselComponent.jsx
+++ src/components/HomeComp/CarouselComponent.jsx
@@ -1,14 +1,15 @@
 import { Carousel } from "flowbite-react";
-
+import Manufacturing from '../../image-org/home/Carousel-images/Manufacturing_Image 2.jpg';
+import Production from '../../image-org/home/Carousel-images/Product Engineering  - Image.jpg';
+import PCB from '../../image-org/home/Carousel-images/Product Engineering Image_2.jpg';
  function CarouselComponent() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96" style={{ height: '500px' }}>
       <Carousel pauseOnHover >
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src={Manufacturing} alt="..." />
+        <img src={Production} alt="..." />
+        <img src={PCB} alt="..." />
+
       </Carousel>
     </div>
   );
